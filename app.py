ffdd23ec0bbb2c8192652c083e060c12b1951205
Mathieu Ract <m.ract@groupeonepoint.com>
2024-05-16 12:33:55 +0200

    version multimodale fichier

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
 import gradio as gr
+import time
 from services.services import RAG_mistralAI
 
 
-demo = gr.ChatInterface(
-        RAG_mistralAI,
-        title="cAIse coach",
-        css="footer {visibility: hidden}",
-        submit_btn="Envoyer",
-        stop_btn="Stop",
-        retry_btn="Re-éssayer",
-        undo_btn="Retour",
-        clear_btn="Nettoyer",
-        fill_height="True",
-        concurrency_limit=None
+def add_message(history, message):
+    for x in message["files"]:
+        history.append(((x,), None))
+    if message["text"] is not None:
+        history.append((message["text"], None))
+    return history, gr.MultimodalTextbox(value=None, interactive=False)
+
+
+def bot(history):
+    response = RAG_mistralAI(history[0][0], history[0][1])['answer']
+    history[-1][1] = ""
+    for character in response:
+        history[-1][1] += character
+        time.sleep(0.02)
+        yield history
+
+
+with gr.Blocks(css="footer{display:none !important}", fill_height=True) as demo:
+    chatbot = gr.Chatbot(
+        [],
+        elem_id="chatbot",
+        bubble_full_width=False,
+        show_copy_button=True
+    )
+
+    chat_input = gr.MultimodalTextbox(
+        interactive=True,
+        file_types=["image"],
+        placeholder="Enter message or upload file...",
+        show_label=False
         )
 
-demo.launch(favicon_path="./data/favicon.ico")
-# demo.launch(favicon_path="./data/favicon.ico", share=True)
+    chat_msg = chat_input.submit(
+        fn=add_message,
+        inputs=[chatbot, chat_input],
+        outputs=[chatbot, chat_input]
+        )
+
+    bot_msg = chat_msg.then(bot, chatbot, chatbot, api_name="bot_response")
+    bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
+
+demo.queue()
+if __name__ == "__main__":
+    demo.launch(favicon_path="./data/favicon.ico")
--- services/services.py
+++ services/services.py
@@ -1,13 +1,15 @@
 import random
+# from openai import OpenAI
+# from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_mistralai.embeddings import MistralAIEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
-from prompts import main_prompt
+from langchain.chains.retrieval import create_retrieval_chain
+from services.prompts import main_prompt
 from dotenv import load_dotenv
 import os
 
@@ -20,21 +22,47 @@ def random_response():
     return random.choice(["Oui", "Non"])
 
 
-def RAG_mistralAI(message, history):
-
-    loader = PyPDFLoader("data/AJ_chemicals.pdf")
+def load_document(file_path):
+    loader = PyPDFLoader(file_path)
     docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter()
     documents = text_splitter.split_documents(docs)
+    return documents
+
+
+def RAG_mistralAI(message, history):
+    documents = load_document("data/AJ_chemicals.pdf")
 
     embeddings = MistralAIEmbeddings(model="mistral-embed", mistral_api_key=MISTRALAI_API_KEY)
     vector = FAISS.from_documents(documents, embeddings)
     retriever = vector.as_retriever()
 
-    model = ChatMistralAI(model="open-mistral-7b", mistral_api_key=MISTRALAI_API_KEY)
+    model = ChatMistralAI(
+        model="open-mistral-7b",
+        mistral_api_key=MISTRALAI_API_KEY)
     prompt = ChatPromptTemplate.from_template(main_prompt)
 
     document_chain = create_stuff_documents_chain(model, prompt)
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
     response = retrieval_chain.invoke({"input": message, "context": history})
-    return response["answer"]
+    return response
+
+
+# def RAG_openAI(message, history):
+#     documents = load_document("data/AJ_chemicals.pdf")
+
+#     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+#     vector = FAISS.from_documents(documents, embeddings)
+#     retriever = vector.as_retriever()
+
+#     model = OpenAI(
+#         model_name="gpt-3.5-turbo",
+#         openai_api_key=OPENAI_API_KEY)
+#     prompt = ChatPromptTemplate.from_template(main_prompt)
+
+#     document_chain = create_stuff_documents_chain(model, prompt)
+#     retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+#     response = retrieval_chain.invoke({"input": message, "context": history})
+#     return response["answer"]
